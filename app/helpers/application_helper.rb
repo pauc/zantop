69ed45b877dcf48526b7e6f55ca6763d4be500a7
Pau Compte <pausandalio@gmail.com>
2012-06-28 14:04:14 +0200

    Initial layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# encoding: utf-8
 module ApplicationHelper
+  def title(title = nil)
+    title = ' :: ' + title unless title.nil?
+    content_for(:title) { title }
+  end
+
+  def body_classes
+    "class='#{controller.controller_name} #{controller.action_name}'"
+  end
 end
