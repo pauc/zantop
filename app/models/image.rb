7d9b23718cb83656d45117e983b54366cc8e7384
Pau Compte <pausandalio@gmail.com>
2012-07-03 23:55:49 +0200

    Image Uploader and config files for carrifor and carrierwave

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,9 @@
 class Image < ActiveRecord::Base
   belongs_to :illustrated, polymorphic: true, dependent: :destroy
 
+  attr_accessible :image
+
   validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
 end
