d259cfe427f02c18ff37dce7c273f1c39061a071
Pau Compte <pausandalio@gmail.com>
2012-08-21 23:20:28 +0200

    Several string translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,22 @@
 # encoding: utf-8
 module ApplicationHelper
   def title(title = nil)
-    title = ' :: ' + title unless title.nil?
+    title = title + ' | ' unless title.nil?
     content_for(:title) { title }
+  end
+
+  def page_title(title)
+    content_for(:page_title) { title.html_safe }
+    title strip_tags(title)
+  end
+
+  def site_name
+    tag = front? ? :h1 : :span
+    content_tag tag, link_to('Zantop', home_path), id: 'site_name'
   end
 
   def body_classes
     "class='#{controller.controller_name} #{controller.action_name}'"
-  end
-
-  def main_title
-    tag = front? ? :h1 : :span
-    content_tag tag, link_to('Zantop', home_path), id: 'main_title'
   end
 
   def front?
@@ -22,6 +27,6 @@ module ApplicationHelper
     ca = I18n.locale == :ca ? 'ca' : link_to('ca', url_for(locale: 'ca'))
     es = I18n.locale == :es ? 'es' : link_to('es', url_for(locale: 'es'))
     en = I18n.locale == :en ? 'en' : link_to('en', url_for(locale: 'en'))
-    "#{ca} | #{en} | #{es}"
+    "<li title='Català'>#{ca}</li> | <li title='English'>#{en}</li> | <li title='Español'>#{es}</li>"
   end
 end
