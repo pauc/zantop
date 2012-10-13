c8b1af5d8736c89e5fa9fc9076f5f5b10cefedea
Pau Compte <pausandalio@gmail.com>
2012-10-13 23:09:08 +0200

    Delete unnecessari specs

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,16 +3,8 @@ require "spec_helper"
 describe UsersController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/#{I18n.locale}/users").should route_to("users#index", locale: I18n.locale.to_s)
-    end
-
     it "routes to #new" do
       get("/#{I18n.locale}/users/new").should route_to("users#new", locale: I18n.locale.to_s)
-    end
-
-    it "routes to #show" do
-      get("/#{I18n.locale}/users/1").should route_to("users#show", id: "1", locale: I18n.locale.to_s)
     end
 
     it "routes to #edit" do
@@ -25,10 +17,6 @@ describe UsersController do
 
     it "routes to #update" do
       put("/#{I18n.locale}/users/1").should route_to("users#update", id: "1", locale: I18n.locale.to_s)
-    end
-
-    it "routes to #destroy" do
-      delete("/#{I18n.locale}/users/1").should route_to("users#destroy", id: "1", locale: I18n.locale.to_s)
     end
   end
 end
