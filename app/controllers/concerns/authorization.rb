ac4822b9ca2fe323d33b0368c9fb242f64c6088c
Pau Compte <pausandalio@gmail.com>
2012-10-13 12:04:40 +0200

    Authorization: non authenticated users can't view unpublished content

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -2,20 +2,29 @@ module Authorization
   extend ActiveSupport::Concern
 
   included do
-    before_filter :authorize, only: [:new,
+    before_filter :authorize_admin, only: [:new,
                                      :edit,
                                      :create,
                                      :update,
                                      :destroy,
                                      :admin]
+
+    before_filter :authorise_unpublished, only: [:show]
   end
 
   private
 
-  def authorize
+  def authorize_admin
     unless current_user?
       store_location
       redirect_to login_path
+    end
+  end
+
+  def authorise_unpublished
+    content = self.controller_name.classify.constantize.find(params[:id])
+    if content.respond_to?('published') and !content.published? and !current_user
+      redirect_to front_path
     end
   end
 
