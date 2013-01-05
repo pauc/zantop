8d5864b1e6aa78127f0f613740dc6d053e8553ad
Pau Compte <pausandalio@gmail.com>
2013-01-05 04:25:31 +0100

    Fix tag-taggings counter_cache

--- app/models/concerns/has_tags.rb
+++ app/models/concerns/has_tags.rb
@@ -6,9 +6,19 @@ module HasTags
     attr_reader :tag_tokens
     has_many :taggings, as: :taggable, dependent: :destroy
     has_many :tags, through: :taggings
+    #before_validation :destroy_taggings
   end
 
   def tag_tokens=(tokens)
-    self.tag_ids = Tag.ids_from_tokens(tokens)
+    tag_ids = Tag.ids_from_tokens(tokens)
+    deleted_tags = self.tag_ids - tag_ids
+    destroy_taggings  deleted_tags, self.id, self.class.table_name.classify.constantize unless deleted_tags.empty?
+    self.tag_ids = tag_ids
+  end
+
+  private
+
+  def destroy_taggings(tag_ids, taggable_id, taggable_type)
+    Tagging.where(tag_id: tag_ids, taggable_id: taggable_id, taggable_type: taggable_type).destroy_all
   end
 end
