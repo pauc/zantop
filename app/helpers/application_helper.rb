e4bc08db36b900739f5a526bf67ce07782c14585
Pau Compte <pausandalio@gmail.com>
2013-03-21 16:19:42 +0100

    Styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,9 +31,9 @@ module ApplicationHelper
   end
 
   def lang_selector
-    ca = link_to_unless_current 'català', url_for(locale: 'ca')
-    es = link_to_unless_current 'castellano', url_for(locale: 'es')
-    en = link_to_unless_current 'english', url_for(locale: 'en')
+    ca = link_to_unless_current 'cat', url_for(locale: 'ca')
+    es = link_to_unless_current 'cast', url_for(locale: 'es')
+    en = link_to_unless_current 'eng', url_for(locale: 'en')
     "<li title='Castellano'>#{es}</li> | <li title='Català'>#{ca}</li> | <li title='English'>#{en}</li>"
   end
 end
