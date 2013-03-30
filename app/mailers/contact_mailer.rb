b2e50b7e631fc5575f78251a9e2e5d73bdea8f45
Pau Compte <pausandalio@gmail.com>
2013-03-30 21:57:14 +0100

    Change email recipient for contact messages

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,8 +5,9 @@ class ContactMailer < ActionMailer::Base
   def contact_message(message)
     @name = message.from_name
     @text = message.text
+    recipient = Rails.env == "production" ? "hola@mireiazantop.com" : "pausandalio@gmail.com"
 
-    mail to: "pausandalio@gmail.com",
+    mail to: recipient,
          subject: "[mireiazantop.com]: #{message.subject}",
          reply_to: message.from_email
   end
