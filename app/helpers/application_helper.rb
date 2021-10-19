d3f64b6e6c5c4f3f0a0b9ff2c92d91aec4b68051
erbdevs <erb.devs@gmail.com>
2021-10-19 20:12:58 +0200

    [13] Fixing site_name part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def site_name
+    tag = front? ? :h1 : :span
+    img_src = front? ? 'MireiaZantop.png' : 'MZ.png'
+    title = front? ? nil : t('home')
+
+    content_tag(
+      tag,
+      link_to_unless_current(
+        image_tag(
+          img_src,
+          alt: "Mireia Zantop"
+        ),
+        front_path,
+        title: title
+      ),
+      id: 'site_name'
+    )
+  end
+
+  def front?
+    controller.controller_name == 'dashboards' && controller.action_name == 'front'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboards/front', as: 'front'
+
   root to: 'dashboards#front'
 end
