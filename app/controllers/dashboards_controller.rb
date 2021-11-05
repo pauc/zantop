9db49200fc6186e292ec740228ba44623baeadd0
erbdevs <87273175+erbdevs@users.noreply.github.com>
2021-11-05 20:12:47 +0100

    [13] Adding common partials to home (#16)
    * [13] Adding some config stuff
    
    * [13] Adding yarn to docker image
    
    * [13] Initial structure for adding front view
    
    * [13] Fixing site_name part
    
    * [13] Adding menu partial
    
    * [13] Bit of styling
    
    * [13] Styles for Main menu
    
    * [13] Using variables
    
    * [13] Remove unused files
    
    * [13] Mixing two csss
    
    * Fixing typo in html
    
    * Cleaning css
    
    * [13] Adding footer
    
    * [13] Adding background color to html
    
    * [13] Fixing rubocop issues
    
    * [13] Remove files added by mistake
    
    * [13] 'Front' page is now called 'Home' page

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,22 @@
+class DashboardsController < ApplicationController
+  # include Authorization
+  # respond_to :html, :js
+
+  # expose(:about_page) { Page.about }
+  # expose(:works) do
+  #   Work.published.includes(:translations).paginate(page: params[:page], per_page: 6)
+  # end
+
+  def home; end
+
+  # def about
+  # respond_with about_page
+  # end
+
+  # def admin
+  # end
+
+  # def provisional
+  # render layout: "empty"
+  # end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def site_name
+    tag = home? ? :h1 : :span
+    img_src = home? ? 'MireiaZantop.png' : 'MZ.png'
+    title = home? ? nil : t('home')
+
+    content_tag(
+      tag,
+      link_to_unless_current(
+        image_tag(
+          img_src,
+          alt: "Mireia Zantop"
+        ),
+        home_path,
+        title: title
+      ),
+      id: 'site_name'
+    )
+  end
+
+  def home?
+    controller.controller_name == 'dashboards' && controller.action_name == 'home'
+  end
 end
