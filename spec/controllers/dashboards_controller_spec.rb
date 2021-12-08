a1593e61ac17d12b682921bd212c783e492f0ed5
erbdevs <87273175+erbdevs@users.noreply.github.com>
2021-12-08 12:54:11 +0100

    23 specs for current status (#24)
    * [23] Spec for ActionWorks controller
    
    * [23] Spec for VisualWorks controller
    
    * [23] Spec for ContactMessages controller
    
    * [23] Spec for Dashboards controller
    
    * [23] Specs for Views: Action works and Visual works

---
+++ spec/controllers/dashboards_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe DashboardsController, type: :controller do
+  describe "GET home" do
+    it do
+      get :home, params: { locale: "en" }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET about" do
+    it do
+      get :about, params: { locale: "en" }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
