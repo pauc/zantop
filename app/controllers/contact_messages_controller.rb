81f8be30dbc7449a993d494989737c00def537f0
erbdevs <erb.devs@gmail.com>
2022-01-05 21:35:22 +0100

    [22] ContactMessages controller and its paths

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
 class ContactMessagesController < ApplicationController
-  def new; end
+  def new
+    @contact_message = ContactMessage.new
+  end
+
+  def create
+    @contact_message = ContactMessage.new(params[:contact_message])
+
+    if @contact_message.valid?
+      ContactMailer.contact_message(@contact_message).deliver
+
+      flash[:notice] = "Missatge enviat correctament. Gràcies!!"
+      redirect_to home_path
+    else
+      render action: 'new'
+    end
+  end
 end
