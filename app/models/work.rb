4ad7dc9568c6fd38f48aa3c8167f8398b4cb3065
erbdevs <erb.devs@gmail.com>
2022-04-07 19:50:08 +0200

    Trying to fix images thing

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Work < ApplicationRecord
+  include HasImages
+
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   has_many :sections, dependent: :destroy
   has_many :images, dependent: :destroy
-
-  include HasImages
 
   translates :title, :description, :techniques, :place
 
