366c6a1bfdd6a871c89d41099a7f5ce67f60f0d6
erbdevs <erb.devs@gmail.com>
2022-04-07 20:31:02 +0200

    Basic pagination

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 class DashboardsController < ApplicationController
   # include Authorization
-  # respond_to :html, :js
 
   def front
-    # @front_works = Work.published.includes(:translations).paginate(page: params[:page], per_page: 6)
-    @front_works = Work.published.includes(:translations)
+    @front_works = Work.published.includes(:translations).paginate(page: params[:page], per_page: 6)
   end
 end
