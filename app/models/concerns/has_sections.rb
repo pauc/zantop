bcba943f14aa48f28ce76940ccf04cfdda420c75
erbdevs <erb.devs@gmail.com>
2022-04-22 12:32:03 +0200

    Sections for works

---
+++ app/models/concerns/has_sections.rb
@@ -0,0 +1,12 @@
+module HasSections
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :sections_attributes
+
+    has_many :sections, as: :content, dependent: :destroy
+
+    accepts_nested_attributes_for :sections, allow_destroy: true,
+      reject_if: proc { |params| params[:body].blank? }
+  end
+end
