8e15c679a9a7b98af3d24150d6b2135a278c7ccb
erbdevs <erb.devs@gmail.com>
2022-04-22 12:45:25 +0200

    Related works working

--- app/controllers/action_works_controller.rb
+++ app/controllers/action_works_controller.rb
@@ -20,8 +20,7 @@ class ActionWorksController < ApplicationController
 
   def show
     @work = ActionWork.find(params[:id])
-    # @related_works = @work.related
-    @related_works = []
+    @related_works = @work.related
 
     flash.now[:alert] = t('untranslated_content') unless @work.has_translation?(I18n.locale)
 
