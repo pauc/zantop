c2415fbb142bcf89f082778cfa4a36142d2d358f
Pau Compte <pausandalio@pm.me>
2022-08-30 00:35:36 +0200

    Rebuild the grid layout for the front page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,9 @@ class DashboardsController < ApplicationController
   # include Authorization
 
   def front
-    @front_works = Work.published.includes(:translations).paginate(page: params[:page], per_page: 6)
+    @front_works = Work
+                   .published
+                   .includes(:translations)
+                   .paginate(page: params[:page], per_page: 12)
   end
 end
