bfc22b19d55f740a60180d1420e2a62262b6bc7c
Pau Compte <pausandalio@pm.me>
2022-09-09 23:55:00 +0200

    Restore the admin bar an start working on forms for action works

--- config/application.rb
+++ config/application.rb
@@ -3,17 +3,13 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,12 +20,6 @@ module Zantop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Make concerns routes visible
-    config.autoload_paths += %W[
-      #{config.root}/app/models/concerns
-      #{config.root}/app/controllers/concerns
-    ]
 
     # Configuration for the application, engines, and railties goes here.
     #
